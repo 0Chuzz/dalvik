65d27f9b31b7154344805b410b25eb478a71319b
chuzz <chuzz.chuzz@gmail.com>
2013-09-14 15:43:58 +0200

    cleanup

--- Dalvik/Apk.hs
+++ Dalvik/Apk.hs
@@ -1,8 +1,6 @@
 module Dalvik.Apk where
 
 import Codec.Archive.Zip
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as LBS
 import Data.Conduit.Binary
 import System.IO
 
@@ -12,8 +10,6 @@ import Dalvik.Types
 loadDexFromApkIO :: FilePath -> IO (Either String DexFile)
 loadDexFromApkIO f = do
   chunks <- withArchive f (sourceEntry "classes.dex" sinkLbs)
-  -- TODO: this is silly. Should we tweak the parser to work with
-  -- lazy ByteStrings?
   return . loadDex $ chunks
 
 loadDexFromAnyIO :: FilePath -> IO (Either String DexFile)
