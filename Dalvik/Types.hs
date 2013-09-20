e11af1583486a8036de6c5086051412972e2896e
chuzz <chuzz.chuzz@gmail.com>
2013-09-20 19:13:11 +0200

    new types module finished

--- Dalvik/Types.hs
+++ Dalvik/Types.hs
@@ -1,25 +1,30 @@
 module Dalvik.Types where
 
-import Data.Set
+import Data.Set (Set)
+import Data.Map (Map)
+import Data.Word (Word8, Word16, Word32, Word64)
 
 import Dalvik.AccessFlags (AccessFlags)
-import Dalvik.Types (CodeItem)
+import Dalvik.RawTypes (CodeItem)
 
 -- http://source.android.com/devices/tech/dalvik/dex-format.html
+-- TODO: string syntax
 
 newtype Type = Type String
 type Interface = Class
 
-type Annotation = String --XXX
 type Code = CodeItem
 
-data Value = VByte Int -- XXX
-           | VShort Int
+data Annotation = Annotation { annotationType :: Type
+                             , annotationElements :: Map String Value}
+
+data Value = VByte Word8
+           | VShort Word16
            | VChar Char
-           | VInt Int
-           | VLong Int
+           | VInt Word32
+           | VLong Word64
            | VFloat Float
-           | VDouble Float
+           | VDouble Double
            | VString String
            | VType Type
            | VField Field
@@ -28,7 +33,7 @@ data Value = VByte Int -- XXX
            | VArray [Value]
            | VAnnotation Annotation
            | VNull
-           | VBoolean Boolean
+           | VBoolean Bool
             
 
 data Field = Field { fieldAccessFlags :: AccessFlags
@@ -57,3 +62,4 @@ data Class = Class { classType :: Type
                    , directMethods :: [Method]
                    , virtualMethods :: [Method]
                    }
+
